611d9164b1b6867e5962b6c2f75200bd715e4f79
tiger <tiger.fdev@gmail.com>
2016-10-21 23:51:36 +0900

    Initial Commit

---
+++ last train/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  last train
+//
+//  Created by 大河渡邉 on 2016/10/21.
+//  Copyright © 2016年 tiger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
