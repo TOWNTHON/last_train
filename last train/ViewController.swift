f025fb29d62ec9614f36da3a77c6d5c886b1ed19
tiger <tiger.fdev@gmail.com>
2016-10-29 16:34:56 +0900

    不要処理削除

--- last train/ViewController.swift
+++ last train/ViewController.swift
@@ -5,19 +5,10 @@
 //
 
 import UIKit
-import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var dispMyTrainStopLabel: UILabel!
-    
-    
-    // 現在地の位置情報の取得にはCLLocationManagerを使用
-    var lm: CLLocationManager!
-    // 取得した緯度を保持するインスタンス
-    var latitude: CLLocationDegrees!
-    // 取得した経度を保持するインスタンス
-    var longitude: CLLocationDegrees!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,24 +29,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         ud.set(MyTrainStopName.text, forKey: "myTrainStopName")
         ud.synchronize()
         dispMyTrainStopLabel.text = ud.object(forKey: "myTrainStopName") as? String
-    }
-    
-    /* 位置情報取得成功時に実行される関数 */
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let newLocation = locations.last
-        // 取得した緯度がnewLocation.coordinate.longitudeに格納されている
-        latitude = newLocation!.coordinate.latitude
-        // 取得した経度がnewLocation.coordinate.longitudeに格納されている
-        longitude = newLocation!.coordinate.longitude
-        // TODO
-        // 取得した緯度・経度をLogに表示
-        NSLog("latiitude: \(latitude) , longitude: \(longitude)")
-        
-        //LastTrain.getNearTrainStop(latitude: latitude, longitude: longitude)
-        //LastTrain.getLastTrainTime()
-        
-        // GPSの使用を停止する．停止しない限りGPSは実行され，指定間隔で更新され続ける．
-        lm.stopUpdatingLocation()
     }
     
 }
