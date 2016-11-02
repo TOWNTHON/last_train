362c66b69e86c65e77cc32d8ecfc7127305a1ba3
Ai Furukawa <ai_furukawa@r.recruit.co.jp>
2016-11-02 23:37:22 +0900

    ルート表示画面のラベルとURLの設定

--- last train/MainViewController.swift
+++ last train/MainViewController.swift
@@ -2,7 +2,7 @@
 //  MainViewController.swift
 //  last train
 //
-//  Created by 古川　愛 on 2016/10/30.
+//  Created by 古川　愛 on 2016/11/02.
 //  Copyright © 2016年 tiger. All rights reserved.
 //
 
@@ -12,10 +12,9 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        nearTrainStop.text = "あいうえお"
+        lastTrainTime.text = "23:40"
         // Do any additional setup after loading the view.
-        let url = URL(string: "http://roote.ekispert.net/sp/result?dep=%E9%8A%80%E5%BA%A7&dep_code=22641&arr=%E8%A5%BF%E5%A4%A7%E5%B3%B6&arr_code=22866&via1=&via1_code=&via2=&via2_code=&yyyymm=201610&day=30&hour=23&minute10=4&minute1=5&type=arr&sort=time&transfer=2&ticket_type=0&surcharge=3&shinkansen=true&local=true&highway=true&plane=true&connect=true&liner=true&ship=true&submit_btn=")
-        let app:UIApplication = UIApplication.shared
-        app.open(url!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +23,16 @@ class MainViewController: UIViewController {
     }
     
 
+    @IBOutlet weak var nearTrainStop: UILabel!
+    
+    @IBOutlet weak var lastTrainTime: UILabel!
+    
+    @IBAction func checkRoot(_ sender: Any) {
+        let url = URL(string: "http://roote.ekispert.net/sp/result?dep=%E9%8A%80%E5%BA%A7&dep_code=22641&arr=%E8%A5%BF%E5%A4%A7%E5%B3%B6&arr_code=22866&via1=&via1_code=&via2=&via2_code=&yyyymm=201610&day=30&hour=23&minute10=4&minute1=5&type=arr&sort=time&transfer=2&ticket_type=0&surcharge=3&shinkansen=true&local=true&highway=true&plane=true&connect=true&liner=true&ship=true&submit_btn=")
+        let app:UIApplication = UIApplication.shared
+        app.open(url!)
+    }
+    
     /*
     // MARK: - Navigation
 
